1caff254d9e6dd94abc616b8493c38cae4982f0c
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-05-31 20:10:59 +0900

    Base Setting

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+// 서버 생성
+var app = http.createServer((request, response) => {
+  var url = request.url;
+  if(request.url == '/'){
+    url = '/index.html';
+  }
+  if(request.url == '/favicon.ico'){
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  response.end(fs.readFileSync(__dirname + url));
+});
+
+// 3000번 포트를 통해 서버 실행
+app.listen(3000);
