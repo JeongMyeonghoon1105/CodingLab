3c59fdf077d0e19682468a1680fc121bce063698
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-06-08 03:23:04 +0900

    Path To Writing Page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/html/index.html");
 });
+app.get('/write', (req, res) => {
+  res.sendFile(__dirname + "/public/html/writing.html");
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT);
