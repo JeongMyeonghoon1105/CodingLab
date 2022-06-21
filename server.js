b9670d88c6eafa83a051e59001e0c2665f07933e
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-06-21 15:31:40 +0900

    URL

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/html/index.html");
 });
+app.get('/dimigo', (req, res) => {
+  res.sendFile(__dirname + "/public/html/dimigo.html");
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT);
