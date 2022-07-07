4b23d6d383245e08a8448f311bc375d3be43d37a
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-07-07 23:31:38 +0900

    CRUD Summernote

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
+// $ npm install --save body-parser
+var bodyParser = require('body-parser')
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/html/index.html");
 });
+
 app.get('/write', (req, res) => {
   res.sendFile(__dirname + "/public/html/writing.html");
+});
+
+app.get('/posting', (req, res) => {
+  res.sendFile(__dirname + "/public/List/post.html");
+});
+
+app.post('/post', (req, res) => {
+  var text = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Summernote Practice</title>
+    <!-- Style -->
+    <link rel="stylesheet" href="/styles.css">
+  </head>
+  <body>
+    <header>
+      <div>Summernote Practice</div>
+    </header>`
+  text = text + req.body.editordata;
+  text = text + `
+  </body>
+  </html>`;
+
+  filePath = __dirname + '/public/List/post.html';
+
+  fs.writeFileSync(filePath, text);
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000
