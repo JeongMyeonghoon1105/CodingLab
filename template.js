a96d48fe9395564c29bbef52592ab7f5be71561b
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-07-19 21:36:39 +0900

    Telephone Number

--- template.js
+++ template.js
@@ -9,6 +9,7 @@ var template = {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
+        <meta name="format-detection" content="telephone=no" />
         <title>동탄코딩랩학원</title>
         <!-- Fonts & Icons -->
         <link rel="icon" type="image/x-icon" href="assets/img/logos/logo.png">
