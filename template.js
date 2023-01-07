028078700d21ea865c9ed1ca0e3aeae0cc2e412d
unknown <wjdaudgns1105@naver.com>
2023-01-07 19:53:52 +0900

    Edit Template

--- template.js
+++ template.js
@@ -166,15 +166,17 @@ var template = {
       <div class="container">
         <div class="row">
           <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-            <h1>Better Solutions For Your Business</h1>
-            <h2>We are team of talented designers making websites with Bootstrap</h2>
+            <h1>코딩랩학원</h1>
+            <h2>세밀한 지도로 최고의 교육을 제공합니다</h2>
             <div class="d-flex justify-content-center justify-content-lg-start">
-              <a href="#about" class="btn-get-started scrollto">Get Started</a>
-              <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+              <a href="#" class="btn-get-started scrollto">Get Started</a>
+              <a href="#" class="glightbox btn-watch-video">
+                <i class="bi bi-play-circle"></i><span>Watch Video</span>
+              </a>
             </div>
           </div>
           <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-            <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+            <img src="Arsha/assets/img/hero-img.png" class="img-fluid animated" alt="">
           </div>
         </div>
       </div>
